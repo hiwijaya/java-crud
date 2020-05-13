51545a08551769d994d01070a355d7d5c9d0c763
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2020-05-13 15:34:19 +0700

    set connection pool with HikariCP

--- src/main/java/com/hiwijaya/crud/datasource/DatabaseHelper.java
+++ src/main/java/com/hiwijaya/crud/datasource/DatabaseHelper.java
@@ -1,6 +1,8 @@
 package com.hiwijaya.crud.datasource;
 
 import com.hiwijaya.crud.util.Lib;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -12,27 +14,52 @@ import java.util.Properties;
  */
 public class DatabaseHelper {
 
+    private static final boolean USE_CONNECTION_POOL = true;
+
+    private static HikariDataSource poolSource;
+
+    // singleton
     private static Connection connection;
 
-    private DatabaseHelper(){
-        // just to prevent instantiate
-    }
+
+    private DatabaseHelper(){ }
+
 
     public static Connection getConnection() throws SQLException {
-        if(connection == null){
 
-            Properties props = Lib.getPropertiesFile("config.properties");
+        if(USE_CONNECTION_POOL){
+            if(poolSource == null){
+                Properties props = Lib.getPropertiesFile("hikari.properties");
 
-            final String host = props.getProperty("HOST");
-            final String user = props.getProperty("USER");
-            final String password = props.getProperty("PASSWORD");
+                HikariConfig config = new HikariConfig(props);
+                poolSource = new HikariDataSource(config);
+            }
 
-            connection = DriverManager.getConnection(host, user, password);
-            System.out.println("Database connected.");
+            return poolSource.getConnection();
+        }
+        else{
+            if(connection == null){
+                Properties props = Lib.getPropertiesFile("config.properties");
+
+                final String host = props.getProperty("HOST");
+                final String user = props.getProperty("USER");
+                final String password = props.getProperty("PASSWORD");
+
+                // init singleton object connection
+                connection = DriverManager.getConnection(host, user, password);
+                System.out.println("Database connected.");
+
+            }
 
             return connection;
         }
-        return connection;
+
+
+
+
+
+
+
     }
 
 }
